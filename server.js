407932641ff902f15ca2212865f428f0846aa53a
Eldow <francois.decan@free.fr>
2016-11-19 06:07:08 -0500

    init repo

---
+++ server.js
@@ -0,0 +1,18 @@
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express')        // call express
+var app        = express()                 // define our app using express
+var port = process.env.PORT || 3000        // set our port
+
+//app.set('view engine', 'pug')
+app.use(express.static(__dirname))
+
+app.get('/', function(req, res){
+	res.render('index')
+})
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Lambda Engine JS running on port : ' + port)
