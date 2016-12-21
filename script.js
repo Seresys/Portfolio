1b9da95437a9955068d173e1b077a8494244bb04
Eldow <francois.decan@free.fr>
2016-12-21 04:06:37 -0500

    Updated mail & add content

--- script.js
+++ script.js
@@ -13,37 +13,47 @@ portfolio.config(function($routeProvider) {
           templateUrl : 'pages/resume.html'
         })
         .when('/contact', {
-          templateUrl : 'pages/contact.html'
+          templateUrl : 'pages/contact.html',
+          controller: 'contactController'
         })
         .when('/projects', {
           templateUrl: 'pages/projects.html'
         })
         .when('/lambda-engine', {
-          templateUrl: 'pages/projects/lambda-engine.html'
+          templateUrl: 'pages/projects/lambda-engine.html',
+          controller: 'lambdaEngineController'
         })
         .when('/the-village', {
-          templateUrl: 'pages/projects/the-village.html'
+          templateUrl: 'pages/projects/the-village.html',
+          controller: 'theVillageController'
         })
         .when('/dynasty', {
-          templateUrl: 'pages/projects/dynasty.html'
+          templateUrl: 'pages/projects/dynasty.html',
+          controller: 'dynastyController'
         })
         .when('/pez-audio-player', {
-          templateUrl: 'pages/projects/pez-audio-player.html'
+          templateUrl: 'pages/projects/pez-audio-player.html',
+          controller: 'pezAudioPlayerController'
         })
         .when('/constellation-series', {
-          templateUrl: 'pages/projects/constellation-series.html'
+          templateUrl: 'pages/projects/constellation-series.html',
+          controller: 'constellationSeriesController'
         })
         .when('/eclipse', {
-          templateUrl: 'pages/projects/eclipse.html'
+          templateUrl: 'pages/projects/eclipse.html',
+          controller: 'eclipseController'
         })
         .when('/hire', {
-          templateUrl: 'pages/projects/hire.html'
+          templateUrl: 'pages/projects/hire.html',
+          controller: 'hireController'
         })
         .when('/jchrome', {
-          templateUrl: 'pages/projects/jchrome.html'
+          templateUrl: 'pages/projects/jchrome.html',
+          controller: 'jChromeController'
         })
         .when('/tsp-solver', {
-          templateUrl: 'pages/projects/tsp-solver.html'
+          templateUrl: 'pages/projects/tsp-solver.html',
+          controller: 'tspSolverController'
         })
         .otherwise({
           redirectTo: '/'
@@ -66,4 +76,57 @@ portfolio.controller('mainController', function($scope, $http) {
     $scope.SetProject = function(id){
       $scope.project = $scope.data.projects.list[id];
     }
+    $scope.GetNumber = function(num) {
+      return new Array(num);
+    }
+});
+
+portfolio.controller('lambdaEngineController', function($scope){
+  $scope.project = $scope.data.projects.list[0];
+});
+portfolio.controller('eclipseController', function($scope){
+  $scope.project = $scope.data.projects.list[1];
+});
+portfolio.controller('jChromeController', function($scope){
+  $scope.project = $scope.data.projects.list[2];
+});
+portfolio.controller('theVillageController', function($scope){
+  $scope.project = $scope.data.projects.list[3];
+});
+portfolio.controller('pezAudioPlayerController', function($scope){
+  $scope.project = $scope.data.projects.list[4];
+});
+portfolio.controller('constellationSeriesController', function($scope){
+  $scope.project = $scope.data.projects.list[5];
+});
+portfolio.controller('tspSolverController', function($scope){
+  $scope.project = $scope.data.projects.list[6];
+});
+portfolio.controller('hireController', function($scope){
+  $scope.project = $scope.data.projects.list[7];
+});
+portfolio.controller('dynastyController', function($scope){
+  $scope.project = $scope.data.projects.list[8];
+});
+portfolio.controller('contactController', function($scope, $http){
+  $scope.message = {};
+  $scope.success = false;
+  $scope.error = false;
+  $scope.SendMessage = function(){
+    $http.post('/send', $scope.message).then(successCallback, errorCallback);
+    function successCallback(){
+      $scope.message = angular.copy({});
+      $scope.mailForm.$setPristine();
+      $scope.mailForm.$setUntouched();
+      $scope.mailForm.$rollbackViewValue();
+      $scope.success = true;
+    }
+    function errorCallback(){
+      $scope.message = angular.copy({});
+      $scope.mailForm.$setPristine();
+      $scope.mailForm.$setUntouched();
+      $scope.mailForm.$rollbackViewValue();
+      $scope.error = true;
+    }
+  }
 });
--- server.js
+++ server.js
@@ -3,15 +3,42 @@
 
 // call the packages we need
 var express    = require('express')        // call express
+var nodemailer = require('nodemailer');		 // call nodemailer
+var bodyParser = require('body-parser');	 // parser for post requests
 var app        = express()                 // define our app using express
 var port = process.env.PORT || 3000        // set our port
+
+// Body parser config
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+// Node mailer config
+var user = process.env.USER;
+var pass = process.env.PASSWORD;
+var smtpTransport = nodemailer.createTransport('smtps://'+user+'%40gmail.com:'+pass+'@smtp.gmail.com');
 
 //app.set('view engine', 'pug')
 app.use(express.static(__dirname))
 app.get('/', function(req, res){
 	res.render('index')
 })
+
+app.post('/send',function(req,res){
+	  var mailOptions={
+	      to : PROCESS.env.USER+"@gmail.com",
+	      subject : req.body.subject,
+	      text : req.body.body
+	  }
+	  smtpTransport.sendMail(mailOptions, function(error, response){
+	  if(error){
+	    res.end("error");
+	  }else{
+	  	res.end("sent");
+	  }
+	});
+});
 // START THE SERVER
 // =============================================================================
 app.listen(port);
-console.log('Portfolio running on port : ' + port)
+console.log('Portfolio running on port : ' + port);
